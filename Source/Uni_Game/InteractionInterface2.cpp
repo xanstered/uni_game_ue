6bcdb5ecf12b50fff0430fe6e55b61fc15f06358
xanstered <gzak2@edu.cdv.pl>
2025-11-11 21:26:35 +0100

    edit

--- Source/Uni_Game/InteractionInterface2.cpp
+++
@@ -1,7 +0,0 @@
-#include "InteractionInterface2.h"
-
-// domyœlna pusta implementacja
-void IInteractionInterface2::PickUp_Implementation(AActor* PlayerCharacter)
-{
-    // klasy implementuj¹ce ten interfejs override'uj¹ tê funkcjê
-}
