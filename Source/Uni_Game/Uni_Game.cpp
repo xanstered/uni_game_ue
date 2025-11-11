846163c0dc0267360c8d3df9ff266b5c1c07783d
xanstered <gzak2@edu.cdv.pl>
2025-11-11 21:24:55 +0100

    edits

--- Source/Uni_Game/Uni_Game.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Uni_Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Uni_Game, "Uni_Game" );
-
-DEFINE_LOG_CATEGORY(LogUni_Game)
--- Source/Uni_Game/Uni_GameGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Uni_GameGameMode.h"
-
-AUni_GameGameMode::AUni_GameGameMode()
-{
-	// stub
-}
